9c712e8893636bfcdd66665b1d18194686717285
chrisduffey <duffeyvt12@gmail.com>
2024-08-04 18:54:42 -0400

    data.js

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const projects = [
+    {
+        title: "Title 1",
+        subtitle: "sTitle 1",
+        description: "description 1",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "Title 2",
+        subtitle: "sTitle 2",
+        description: "description 2",
+        image: "./project-2.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "Title 3",
+        subtitle: "sTitle 3",
+        description: "description 3",
+        image: "./project-3.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "Title 4",
+        subtitle: "sTitle 4",
+        description: "description 4",
+        image: "./project-4.gif",
+        link: "https://reactbootcamp.com",
+    },
+
+];
